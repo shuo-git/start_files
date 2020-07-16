6131d9e52c000fdf68610fa1fc01f2771edcf320
shuo-git <315665262@qq.com>
2020-07-16 17:51:32 +0800

    Update start.sh

--- gen_base/start.sh
+++ gen_base/start.sh
@@ -18,7 +18,7 @@ fi
 DISK2=/apdcephfs/share_916081/vinceswang
 DISK_CKP=$DISK2/exp
 DISK_RESULTS=$DISK2/results
-EXP=${DATA}_base_reduce_inference_ece-6
+EXP=${DATA}_base_reduce_inference_ece-10
 DECODE_PATH=$DISK_RESULTS/$EXP/inference
 mkdir -p $DECODE_PATH
 
