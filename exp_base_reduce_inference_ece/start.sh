2a5b90ea3bacb97d285323b56d9ddea884ccb80f
shuo-git <315665262@qq.com>
2020-07-16 10:26:58 +0800

    Update start.sh

--- exp_base_reduce_inference_ece/start.sh
+++ exp_base_reduce_inference_ece/start.sh
@@ -100,7 +100,7 @@ gen(){
 train(){
     # usage: train iteration
     ITE=$1
-    k=5
+    k=3
     ((max_update=ITE*k))
     if [ "$ITE" = "1" ]
     then
@@ -115,8 +115,8 @@ train(){
       --load-TER \
       --lr 0.0001 --lr-scheduler fixed --force-anneal 1 --lr-shrink 0.9 \
       --weight-decay 0.0 --clip-norm 0.0 --dropout 0.1 \
-      --max-sentences 160 \
-      --update-freq 5 \
+      --max-sentences 150 \
+      --update-freq 20 \
       --arch transformer \
       --optimizer adam --adam-betas '(0.9, 0.98)' \
       --tensorboard-logdir $LOG_PATH \
