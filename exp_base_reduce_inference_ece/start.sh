3319a1f55ad1a3e616b07152f28d4f8b40dd04d0
shuo-git <315665262@qq.com>
2020-07-17 15:42:32 +0800

    Update start.sh

--- exp_base_reduce_inference_ece/start.sh
+++ exp_base_reduce_inference_ece/start.sh
@@ -20,7 +20,7 @@ if [ $? != 0 ]; then
 fi
 
 DATA=wmt14_en_de_stanford_devtest
-EXP=wmt14_en_de_stanford_base_reduce_inference_ece-15
+EXP=wmt14_en_de_stanford_base_reduce_inference_ece-18
 CALI=$DISK1/code/Cali-Ana
 InfECE=$DISK1/code/InfECE
 TER=$DISK1/tools/tercom-0.7.25
@@ -121,14 +121,14 @@ train(){
       --optimizer adam --adam-betas '(0.9, 0.98)' \
       --tensorboard-logdir $LOG_PATH \
       --criterion label_smoothed_cross_entropy_inference_ece \
-      --label-smoothing 0.0 --num-bins 20 --ece-scale 10.0 --ece-alpha 0.5 \
+      --label-smoothing 0.0 --num-bins 20 --ece-scale 10.0 --ece-alpha 0.0 \
       --wrong-token-weight 0.0 \
       --no-progress-bar \
       --log-format simple \
       --log-interval 1 \
       --save-interval-updates 10000 \
       --save-interval 1 \
-      --validate-interval 1 \
+      --validate-interval 10000 \
       --max-update ${max_update} \
       --beam 1 \
       --remove-bpe \
