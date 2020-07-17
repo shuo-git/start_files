818e8beb22d2ac5a58fe49a000aeb4f572e8d261
shuo-git <315665262@qq.com>
2020-07-17 15:31:24 +0800

    Update start.sh

--- exp_base_reduce_inference_ece/start.sh
+++ exp_base_reduce_inference_ece/start.sh
@@ -20,7 +20,7 @@ if [ $? != 0 ]; then
 fi
 
 DATA=wmt14_en_de_stanford_devtest
-EXP=wmt14_en_de_stanford_base_reduce_inference_ece-14
+EXP=wmt14_en_de_stanford_base_reduce_inference_ece-15
 CALI=$DISK1/code/Cali-Ana
 InfECE=$DISK1/code/InfECE
 TER=$DISK1/tools/tercom-0.7.25
@@ -122,7 +122,7 @@ train(){
       --tensorboard-logdir $LOG_PATH \
       --criterion label_smoothed_cross_entropy_inference_ece \
       --label-smoothing 0.0 --num-bins 20 --ece-scale 10.0 --ece-alpha 0.5 \
-      --wrong-token-weight 0.5 \
+      --wrong-token-weight 0.0 \
       --no-progress-bar \
       --log-format simple \
       --log-interval 1 \
