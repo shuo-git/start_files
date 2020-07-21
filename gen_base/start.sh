35031ce64482fcc6aa7aaec46ea88459db2498e7
shuo-git <315665262@qq.com>
2020-07-21 20:41:59 +0800

    Update start.sh

--- gen_base/start.sh
+++ gen_base/start.sh
@@ -18,7 +18,7 @@ fi
 DISK2=/apdcephfs/share_916081/vinceswang
 DISK_CKP=$DISK2/exp
 DISK_RESULTS=$DISK2/results
-EXP=${DATA}_base_reduce_inference_ece-20-3
+EXP=${DATA}_base_reduce_inference_ece-24
 DECODE_PATH=$DISK_RESULTS/$EXP/inference
 mkdir -p $DECODE_PATH
 
@@ -28,7 +28,7 @@ mkdir -p $DECODE_PATH
 #   --num-update-checkpoints $N
 # done
 
-for beam in 100;do
+for beam in 100 4;do
 for da in 0.5 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5;do
 if [[ $beam = 4 ]]; then
   bsz=128
@@ -38,42 +38,7 @@ else
   bsz=2
 fi
 
-for step in checkpoint7;do
-echo ${step}
-CP=${step}.pt
-CHECKPOINT=$DISK_CKP/$EXP/$CP
-for SUBSET in test;do
-GEN=${SUBSET}_${step}.${beam}.${da}.gen
-echo "Evaluate on $DATA/$SUBSET with $CHECKPOINT"
-CUDA_VISIBLE_DEVICES=0 python3.6 $DISK_CODE/generate.py \
-  $DISK_DATA/$DATA/data-bin \
-  --fp16 \
-  -s en \
-  -t de \
-  --path $CHECKPOINT \
-  --gen-subset $SUBSET \
-  --lenpen ${da} \
-  --beam ${beam} \
-  --max-sentences $bsz \
-  > $DECODE_PATH/${GEN}
-
-sh $DISK_CODE/scripts/compound_split_bleu.sh $DECODE_PATH/${GEN}
-done
-done
-done
-done
-
-for beam in 4;do
-for da in 0.5 1.1 1.2 1.3 1.4 1.5;do
-if [[ $beam = 4 ]]; then
-  bsz=128
-elif [[ $beam = 100 ]]; then
-  bsz=20
-else
-  bsz=2
-fi
-
-for step in checkpoint7;do
+for step in checkpoint5;do
 echo ${step}
 CP=${step}.pt
 CHECKPOINT=$DISK_CKP/$EXP/$CP
