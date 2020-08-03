b46657a5134bfc18eb944f2bb4fde5ece7568dc9
shuo-git <315665262@qq.com>
2020-08-03 10:38:56 +0800

    Update start.sh

--- gen_base/start.sh
+++ gen_base/start.sh
@@ -18,7 +18,8 @@ fi
 DISK2=/apdcephfs/share_916081/vinceswang
 DISK_CKP=$DISK2/exp
 DISK_RESULTS=$DISK2/results
-EXP=${DATA}_base-ls-0
+for exp_i in 0 2 3 4 5 6;do
+EXP=${DATA}_base-ls-${exp_i}
 DECODE_PATH=$DISK_RESULTS/$EXP/inference
 mkdir -p $DECODE_PATH
 
@@ -30,7 +31,7 @@ mkdir -p $DECODE_PATH
 
 for beam in 1;do
 for da in 0.0 1.0;do
-for t in 1.1 1.2 1.3 1.4 1.5;do
+for t in 1.0;do
 if [[ $beam = 1 ]]; then
   bsz=512
 elif [[ $beam = 4 ]]; then
@@ -71,6 +72,7 @@ CUDA_VISIBLE_DEVICES=0 python3.6 $DISK_CODE/generate.py \
   > $DECODE_PATH/${GEN}
 
 sh $DISK_CODE/scripts/compound_split_bleu.sh $DECODE_PATH/${GEN} > $DECODE_PATH/${GEN}.bleu
+done
 done
 done
 done
